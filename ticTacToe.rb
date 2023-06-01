9ad8813f28205b67f59a031c3e5cd2ed15425936
psvkushal <psvkushal@gmail.com>
2023-06-01 22:01:10 +0530

    seperates gameBoard class and main function of the code

--- ticTacToe.rb
+++ ticTacToe.rb
@@ -124,25 +124,3 @@ class GameBoard
   end
   
 end
-puts "enter the name of the first player  : "
-player1 = gets.chomp
-puts "enter the name of the second player : "
-player2 = gets.chomp
-
-newGame = GameBoard.new(player1, player2)
-
-isBoardFull = false
-isGameOver = false
-
-while !isGameOver && !isBoardFull
-  isGameOver = newGame.playTurn
-  isBoardFull = newGame.isBoardFull?
-end
-
-if isGameOver 
-  puts "The winner is #{newGame.player[1 - newGame.currentPlayer]} won the game"
-end
-
-if !isGameOver && isBoardFull 
-  puts "The match is a draw"
-end
